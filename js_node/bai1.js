f2268cb6928812ce417bb5dcd419a6841344f1c6
tuyennx605 <tuyennx605@gmail.com>
2018-12-18 15:38:55 +0700

    bat dau

---
+++ js_node/bai1.js
@@ -0,0 +1,43 @@
+
+//bien
+const tenbien1 = 4; //khong the thay doi
+
+
+//var
+/*
+    co the khai bao chong len nhau duoc
+    co the dung moi noi
+*/
+// var tenbien3 = 1;
+
+// for(let i=0; i<10; i++){
+//    var tenbien3 = 67;
+// }
+
+// console.log(tenbien3);
+
+
+//let
+/*
+    khong the khai bao chong len nhau duoc
+    chi su dung trong pham vi ham
+*/
+for(let i=0; i<10; i++){
+    let tenbien2;
+}
+
+
+console.log(tenbien2);
+
+
+
+
+//ghi chu
+//in ra man hinh
+// console.log("ok");
+
+/*
+dong 1
+dong 2
+dong 3
+*/
---
+++ js_node/bai2.js
@@ -0,0 +1,55 @@
+
+//toan tu 3 ngoi:           dieukien ? bieu thuc dung : bieu thuc sai
+(3==3)? console.log('dung') : console.log('sai');
+
+/*
+//toan tu quan he: ==, ===, !=, !==, > , <, <=, >=
+var result;
+result = 3 == "3"; // result = true
+result = 3 != 3; // result = false
+result = 3 === "3"; // result = false
+result = 3 !== "3"; // result = true
+result = 3 > 4; // result = false
+result = 3 < 4; // result = true
+result = 3 >= 3; // result = true
+result = 3 <= 4; // result = true
+*/
+
+
+/*
+//toan tu logig : && , || , !
+let x=2, y=5;
+let kq;
+
+kq=(x==3) && (y==5);  //false
+kq=(x==3) || (y==5); //true
+kq = !(x==2);  //false
+
+console.log(kq);
+*/
+
+
+
+/*
+//toan tu tang giam  ++, --
+var x = 2;
+var y;
+y = x++; // x = 3, y = 2
+y = ++x; // x = 3, y = 3
+y = x--; // x = 1, y = 2
+y = --x; // x = 1, y = 1
+*/
+
+
+
+/*
+//toan tu so hoc: cộng (+), trừ (-), nhân (*), chia (/), chia dư (%):
+let kq;
+kq = 4 + 3;  //7
+kq = 6-3; //3
+kq = 3*4; //12
+kq = 4/3;  //1.3333333333333333
+kq = 4%3; //1
+
+console.log(kq);
+*/
