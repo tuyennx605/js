9780021cd6dfc4e724df4a5c7438f54c2c78ae64
tuyennx605 <tuyennx605@gmail.com>
2019-01-04 00:56:24 +0700

    b3 4

---
+++ js_node/bai3.js
@@ -0,0 +1,22 @@
+//cac kieu du lieu
+
+
+/*
+    
+    null
+    undefined
+    random
+    boolean
+    error
+
+    ham (function)
+
+    array
+    string
+    number
+    date
+    object
+    class
+
+    map, set
+*/
---
+++ js_node/bai4.js
@@ -0,0 +1,36 @@
+/*
+NOTE: xem kiểu:   typeof(biến) = number - số , string - chuỗi, object - đối tượng, undefined - không xác định
+-Boolean: let x = true; let y = 0;
+-Error: new Error("loi")
+-random: Math.floor(Math.random() * 10) + 1;  // returns a random integer from 1 to 10
+
+-null: là giá trị mình gán : vd: let a = null;
+-undefined: la kieu:  khi khai báo nhưng chưa gán giá trị
+*/
+
+
+/*
+let b = false;
+console.log(typeof(b)==='boolean');
+*/
+
+/*
+// err
+let e = new Error('loi roi nhe!!!');
+console.log(e + '');
+*/
+
+/*
+let r = Math.floor(Math.random()*10)+1;
+console.log(r);
+*/
+
+/*
+let a = null;
+console.log(a);
+*/
+
+/*
+let a;
+console.log(typeof(a)==='undefined');
+*/
